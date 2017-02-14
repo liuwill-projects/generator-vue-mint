e02081e6383ff5192859483154d0ba51bf047cfd
liuwill <liuwei_will@qq.com>
2017-02-14 19:58:36 +0800

    [add] mint ui run successfully

---
+++ app/templates/client/components/main.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="h5-main">
+      <mt-tabbar v-model="selected">
+        <mt-tab-item id="tab1">
+          <i class="fa fa-comment" aria-hidden="true"></i>
+          tab1
+        </mt-tab-item>
+        <mt-tab-item id="tab2">
+          <i class="fa fa-users" aria-hidden="true"></i>
+          tab2
+        </mt-tab-item>
+        <mt-tab-item id="tab3">
+          <i class="fa fa-compass" aria-hidden="true"></i>
+          tab3
+        </mt-tab-item>
+        <mt-tab-item id="tab4">
+          <i class="fa fa-user-o" aria-hidden="true"></i>
+          tab4
+        </mt-tab-item>
+      </mt-tabbar>
+    </div>
+</template>
+<script>
+
+  /*import { Toast } from 'mint-ui';
+  import { Header } from 'mint-ui';
+  import { MessageBox } from 'mint-ui';
+  import { Loadmore } from 'mint-ui';
+  import moment from "moment";
+  import { Popup } from 'mint-ui';
+  */
+
+  export default {
+    data() {
+
+      return {
+        selected:'tab1'
+      };
+    },
+    created(){
+    },
+    mounted() {
+      
+    },
+    methods: {
+      
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
--- app/templates/webpack.js
+++ app/templates/webpack.js
@@ -38,6 +38,7 @@ var myWebpackConfig = {
         loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) 
       },
       { test: /\.json$/, loader: "json-loader" },
+      //{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
       {
           test   : /\.(ttf|eot|svg|svg|woff|woff(2))(\?t\=[0-9]+)?$/,
           loader : 'file-loader'
